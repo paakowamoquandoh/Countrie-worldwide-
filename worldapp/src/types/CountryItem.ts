cb936a4f5386e4fbe5526875e5b13222c47b4b3d
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-07-14 13:12:58 +0000

    updates commit

--- worldapp/src/types/CountryItem.ts
+++
@@ -1,7 +0,0 @@
-export interface CountryItemTS {
-    name: string,
-    capital: string,
-    population: number,
-    region: string,
-    flag: string
-}
--- worldapp/src/types/Input.ts
+++
@@ -1,4 +0,0 @@
-export interface InputTS {
-  value: string;
-  setSearch: (e: string) => void;
-}
